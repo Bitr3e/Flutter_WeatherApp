eab9eb3f44dd5b5496d1490af6be8ca5d284d1d0
John Brence S. Condesa <144252928+Bitr3e@users.noreply.github.com>
2026-05-05 01:46:52 +0800

    added new sec_key

--- lib/services/map_service.dart
+++ lib/services/map_service.dart
@@ -1,5 +1,5 @@
 class MapService {
-  static const String openWeatherApiKey = "";
+  static const String openWeatherApiKey = "ac4a2adc2d23bbee1635725559ad7ced";
 
   static String getTileUrl(String layer, int x, int y, int zoom) {
     return "https://tile.openweathermap.org/map/$layer/$zoom/$x/$y.png?appid=$openWeatherApiKey";
